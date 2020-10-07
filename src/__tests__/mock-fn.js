336dc012740ed6ffd79f5ac233d1a27e974e9b00
KAHLAOUI Mathieu <mathieu.kahlaoui.pro@gmail.com>
2020-10-07 21:03:01 +0200

    mocking with jest fn

--- src/__tests__/mock-fn.js
+++ src/__tests__/mock-fn.js
@@ -25,9 +25,11 @@ test('returns winner', () => {
   const winner = thumbWar('Kent C. Dodds', 'Ken Wheeler')
   expect(winner).toBe('Kent C. Dodds')
   
-  console.log(utils.getWinner.mock);
-  // expect.(utils.getWinner).
   // Your code:
+  expect(utils.getWinner.mock.calls).toEqual([
+    [ 'Kent C. Dodds', 'Ken Wheeler' ],
+    [ 'Kent C. Dodds', 'Ken Wheeler' ]
+  ])
   expect(utils.getWinner).toHaveBeenCalledTimes(2);
   expect(utils.getWinner).toHaveBeenCalledWith("Kent C. Dodds", "Ken Wheeler")
   expect(utils.getWinner).toHaveBeenNthCalledWith(1, "Kent C. Dodds", "Ken Wheeler")
